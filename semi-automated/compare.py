865e7a3aa3c4206887fd59237dd9bc18170fc049
y2mk1ng <aylguo@outlook.com>
2022-03-12 23:28:20 +0800

    add some comments

--- semi-automated/compare.py
+++ semi-automated/compare.py
@@ -1,4 +1,8 @@
-import csv, os, pandas
+'''
+Python version: 3.8
+Author: Yanling Guo (y2mk1ng)
+'''
+import pandas
 csv_1 = input('The file name of your first .csv file: ')
 csv_2 = input('The file name of your second .csv file: ')
 data_1 = set(pandas.read_csv(csv_1)['Name'].tolist())
