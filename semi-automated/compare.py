3c8593f3df9e37777a91cc175bd8583c53ed2b21
y2mk1ng <aylguo@outlook.com>
2022-03-12 21:23:21 +0800

    modified a lot of things

---
+++ semi-automated/compare.py
@@ -0,0 +1,8 @@
+import csv, os, pandas
+csv_1 = input('The file name of your first .csv file: ')
+csv_2 = input('The file name of your second .csv file: ')
+data_1 = set(pandas.read_csv(csv_1)['Name'].tolist())
+data_2 = set(pandas.read_csv(csv_2)['Name'].tolist())
+#print('Name_1: ', data_1)
+diff = data_1.difference(data_2)
+print(diff)
