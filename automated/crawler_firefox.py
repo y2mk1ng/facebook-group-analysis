a4570605942f577c19b6266e13bb396960716e56
y2mk1ng <aylguo@outlook.com>
2022-03-22 21:45:47 +0800

    copy and paste from source

--- automated/crawler_firefox.py
+++ automated/crawler_firefox.py
@@ -60,6 +60,14 @@ for index in addr_list:
         for j in soup.find('a', "oajrlxb2 gs1a9yip g5ia77u1 mtkw9kbi tlpljxtp qensuy8j ppp5ayq2 goun2846 ccm00jje s44p3ltw mk2mc5f4 rt8b4zig n8ej3o3l agehan2d sk4xxmp2 rq0escxv nhd2j8a9 mg4g778l pfnyh3mw p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x tgvbjcpo hpfvmrgz jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso l9j0dhe7 i1ao9s8h esuyzwwr f1sip0of du4w35lb n00je7tq arfg74bv qs9ysxi8 k77z8yql btwxx1t3 abiwlrkh p8dawk7l q9uorilb lzcic4wl")['content']:
             print(j)
     '''
-    bm = driver.find_elements(By.CLASS_NAME, 'b20td4e0 muag1w35')
-    soup = BeautifulSoup(driver.page_source)
-    print(soup)
+    bm = driver.find_element(By.CLASS_NAME, 'b20td4e0 muag1w35')
+    #soup = BeautifulSoup(driver.page_source)
+    #print(soup)
+    bm.send_keys(Keys.CONTROL)
+    bm.send_keys('a')
+    bm.send_keys(Keys.CONTROL)
+    bm.send_keys('c')
+    bm.send_keys(Keys.TAB)
+    with open('member_20220322.txt', 'w') as f:
+        bm.send_keys(Keys.CONTROL)
+        bm.send_keys('v')
